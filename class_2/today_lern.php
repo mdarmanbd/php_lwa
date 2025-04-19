7bc9a338d3538326a6d331fe77372cdbfdadd792
arman_hossain <md.armantext@gmail.com>
2025-04-19 20:13:19 +0600

    learn syntext in php

---
+++ class_2/today_lern.php
@@ -0,0 +1,27 @@
+<?php
+
+// var_dump();
+// is_str, is_array, is_object, is_float;
+// type casting, (string), (array), (object);
+// strlen, 
+// strtoupper, 
+// strtolower, 
+// ucwords, 
+// trim, 
+// tr_replace, 
+// substr, 
+// printf,
+// str_repeat,
+// str_word_count,
+// strrev,
+// strpos,
+// strip_tags
+// is_nan
+// is_numeric
+// min
+// max
+// round()
+// rand()
+// ceil()
+// floor()
+
